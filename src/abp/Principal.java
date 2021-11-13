6a375afdd2da1caaa74fa33ae3a0877001e55396
Jhonda14 <jhonda045@gmail.com>
2021-11-13 08:22:39 -0500

    mi primer commit

---
+++ src/abp/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abp;
+/**
+ *
+ * @author Jhonda14
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaABP Ventana = new VentanaABP();
+        Ventana.setLocationRelativeTo(null);
+        Ventana.setVisible(true);
+        
+                
+    }
+    
+}
---
+++ src/abp/vehiculos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abp;
+
+/**
+ *
+ * @author Jhonda14
+ */
+class vehiculos {
+                int id;
+            String password;
+            String Vehícul;
+            String Placa;
+            String marca;
+            String modelo;
+            String version;
+            String color;
+            String numPuestos;
+            String numPuertas;
+            String combustible;
+            String kilometros;
+            String cilindraje;
+            String categoría;
+}
